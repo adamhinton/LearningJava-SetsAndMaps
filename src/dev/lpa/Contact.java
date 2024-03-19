548694d0abc638529b067a2279a49b38cd2fed64
adamd <adam.d.hinton@gmail.com>
2024-03-18 23:49:11 -0400

    ContactData

--- src/dev/lpa/Contact.java
+++ src/dev/lpa/Contact.java
@@ -35,6 +35,24 @@ public class Contact {
         }
     }
 
+    public String getName() {
+        return name;
+    }
 
 
+    @Override
+    public String toString() {
+        // Java really needs template literals
+        return "%s: %s %s".formatted(name, emails, phones);
+    }
+
+    public Contact mergeContactData (Contact contact){
+        Contact newContact = new Contact(name);
+        newContact.emails = new HashSet<>(this.emails);
+        newContact.phones = new HashSet<>(this.phones);
+        // Only adds non-dupes
+        newContact.emails.addAll(contact.emails);
+        newContact.phones.addAll(contact.phones);
+        return newContact;
+    }
 }
