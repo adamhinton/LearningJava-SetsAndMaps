1f4d0f0054e59ececed92d0052126afcaf61d78b
adamd <adam.d.hinton@gmail.com>
2024-03-18 23:57:34 -0400

    Main.printData

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,18 @@
 package dev.lpa;
 
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
 
 
 
+    }
+
+    public static void printData (String header, Collection<Contact> contacts){
+        System.out.println("-".repeat(30));
+        System.out.println(header);
+        System.out.println("-".repeat(30));
+        contacts.forEach(System.out::println);
     }
 }
